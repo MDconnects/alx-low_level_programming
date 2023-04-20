be98698041c90a4e6da859d42287913efee8255b
MDconnects <md.copyconnect@gmail.com>
2023-04-20 15:56:48 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+ * main - it outputs the phrase with proper grammar,
+ * but the outcome is a piece of art,
+ * Return: Always 0 is exited, otherwise return a non zero int
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
